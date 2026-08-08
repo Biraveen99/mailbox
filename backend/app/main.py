884a61634b9b62180e22abcd656f9d9278e986a3
Biraveen Nedunchelian <biraveennedunchelian@Biraveens-MacBook-Air.local>
2026-08-08 20:06:03 +0200

    backend stuff

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.models import MessageCreate
 
 app = FastAPI()
 
@@ -6,3 +7,11 @@ app = FastAPI()
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+
+@app.post("/messages")
+def create_message(message: MessageCreate):
+    return {
+        "title": message.title,
+        "body": message.body
+    }
